d44363cf41814de5a343503d369b84e82480f1ba
Daniel Herrera <daniel97hh@gmail.com>
2022-01-20 17:01:05 -0500

    app casi ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+movies = URI.open("http://tmdb.lewagon.com/movie/top_rated").read
+
+movies = JSON.parse(movies)
+movies["results"].each do |movie|
+
+  if movie["original_language"] == "en"
+    new_movie = Movie.new
+    new_movie.title = movie["original_title"]
+    new_movie.overview = movie["overview"]
+    new_movie.poster_url = "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}"
+    new_movie.rating = movie["vote_average"]
+    new_movie.save
+  end
+end
